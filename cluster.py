bfd7c4eeac2bd0b82c280cb0a86747390c26c15b
carterrxavier <carter.r.xavier@gmail.com>
2021-06-23 21:19:23 -0500

    more exploration

---
+++ cluster.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+
+
+import wrangle, scale
+
+from sklearn.cluster import KMeans
--- explore.py
+++ explore.py
@@ -13,10 +13,16 @@ def get_distribution(df):
         df[i].hist(grid = False, bins = 100)
         plt.show()
         
+def compare_to_target(df, target):
+    for i in df:
+        plt.figure(figsize=(8,3))
+        plt.title('{} vs {}'.format(i,target))
+        sns.scatterplot(data=df , y = target, x = i)
+        plt.show()
+        
 def plot_variable_pairs(df, cont_vars = 2):
     combos = itertools.combinations(df,cont_vars)
     for i in combos:
         plt.figure(figsize=(8,3))
         sns.regplot(data=df, x=i[0], y =i[1],line_kws={"color":"red"})
         plt.show()
-
